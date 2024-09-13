42c70705856ad817a9be2808d99020d82e3697b5
tom8531400 <134759040+tom8531400@users.noreply.github.com>
2024-09-13 19:00:49 +0800

    feet: 創建專案 h2

---
+++ src/main/java/com/example/coindesksprigboot/CoindeskSprigbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.coindesksprigboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoindeskSprigbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoindeskSprigbootApplication.class, args);
+    }
+
+}
